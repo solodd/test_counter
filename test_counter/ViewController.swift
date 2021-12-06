5c83c5c3e590d92487facd664e9a5457a9e1d035
solodovchenko <solodovchenko@bifit.com>
2021-12-06 20:18:07 +0300

    Initial Commit

---
+++ test_counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_counter
+//
+//  Created by Alex Solod on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
