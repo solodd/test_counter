c4863bb37f4690624ebb5ef0713819f5854cd340
solodd <54677613+solodd@users.noreply.github.com>
2021-12-06 21:41:45 +0300

    1

--- test_counter/ViewController.swift
+++ test_counter/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    
+    @IBOutlet var label: UILabel!
+    var a = 0
+   
+    @IBAction func tap(_ sender: UIButton) {
+        
+         a = a+1
+                    label.text = "\(a)"
+                     if a > 4 {
+                         a = 0
+                         label.text = "\(a)"
+                         let alert = UIAlertController  (title: "Warning", message:   "a > 5", preferredStyle: .alert)
+                         let okButton = UIAlertAction (title: "OK", style: .default, handler: nil)
+                         alert.addAction(okButton)
+                         
+                         present(alert, animated: true, completion: nil)
+                         
+                     }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-
+       
+                
+        label.text = "\(a)"
+        
+            
+        }
+        
 }
-
